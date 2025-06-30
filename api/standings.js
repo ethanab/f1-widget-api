5814fe7a8eab68918caa3c3c4d41443315e488b1
TheWhiteHawk <109964884+ethanab@users.noreply.github.com>
2025-06-30 21:18:38 +0200

    Update standings.js vff

--- api/standings.js
+++ api/standings.js
@@ -1,24 +1,80 @@
 const teamAssets = {
-  "Red Bull": { logo: "...", car: "...", color: "#1E41FF" },
-  "Mercedes": { logo: "...", car: "...", color: "#00D2BE" },
-  "Ferrari": { logo: "...", car: "...", color: "#E80020" },
-  "McLaren": { logo: "...", car: "...", color: "#FF8700" },
-  "Aston Martin": { logo: "...", car: "...", color: "#006F62" },
-  "Alpine F1 Team": { logo: "...", car: "...", color: "#2293D1" },
-  "Visa Cash App RB": { logo: "...", car: "...", color: "#6692FF" },
-  "Williams": { logo: "...", car: "...", color: "#37BEDD" },
-  "Stake F1 Team Kick Sauber": { logo: "...", car: "...", color: "#52E252" },
-  "Haas F1 Team": { logo: "...", car: "...", color: "#B6BABD" }
+  "Red Bull": {
+    logo: "https://media.formula1.com/content/dam/fom-website/teams/2024/red-bull-racing-logo.png",
+    car: "https://media.formula1.com/content/dam/fom-website/manual/Misc/2024manual/Launch/RedBull.jpg",
+    color: "#1E41FF"
+  },
+  "Mercedes": {
+    logo: "https://media.formula1.com/content/dam/fom-website/teams/2024/mercedes-logo.png",
+    car: "https://media.formula1.com/content/dam/fom-website/manual/Misc/2024manual/Launch/Mercedes.jpg",
+    color: "#00D2BE"
+  },
+  "Ferrari": {
+    logo: "https://media.formula1.com/content/dam/fom-website/teams/2024/ferrari-logo.png",
+    car: "https://media.formula1.com/content/dam/fom-website/manual/Misc/2024manual/Launch/Ferrari.jpg",
+    color: "#E80020"
+  },
+  "McLaren": {
+    logo: "https://media.formula1.com/content/dam/fom-website/teams/2024/mclaren-logo.png",
+    car: "https://media.formula1.com/content/dam/fom-website/manual/Misc/2024manual/Launch/McLaren.jpg",
+    color: "#FF8700"
+  },
+  "Aston Martin": {
+    logo: "https://media.formula1.com/content/dam/fom-website/teams/2024/aston-martin-logo.png",
+    car: "https://media.formula1.com/content/dam/fom-website/manual/Misc/2024manual/Launch/AstonMartin.jpg",
+    color: "#006F62"
+  },
+  "Alpine F1 Team": {
+    logo: "https://media.formula1.com/content/dam/fom-website/teams/2024/alpine-logo.png",
+    car: "https://media.formula1.com/content/dam/fom-website/manual/Misc/2024manual/Launch/Alpine.jpg",
+    color: "#2293D1"
+  },
+  "Visa Cash App RB": {
+    logo: "https://media.formula1.com/content/dam/fom-website/teams/2024/visa-cash-app-rb-logo.png",
+    car: "https://media.formula1.com/content/dam/fom-website/manual/Misc/2024manual/Launch/VisaCashAppRB.jpg",
+    color: "#6692FF"
+  },
+  "Williams": {
+    logo: "https://media.formula1.com/content/dam/fom-website/teams/2024/williams-logo.png",
+    car: "https://media.formula1.com/content/dam/fom-website/manual/Misc/2024manual/Launch/Williams.jpg",
+    color: "#37BEDD"
+  },
+  "Stake F1 Team Kick Sauber": {
+    logo: "https://media.formula1.com/content/dam/fom-website/teams/2024/stake-kick-sauber-logo.png",
+    car: "https://media.formula1.com/content/dam/fom-website/manual/Misc/2024manual/Launch/KickSauber.jpg",
+    color: "#52E252"
+  },
+  "Haas F1 Team": {
+    logo: "https://media.formula1.com/content/dam/fom-website/teams/2024/haas-logo.png",
+    car: "https://media.formula1.com/content/dam/fom-website/manual/Misc/2024manual/Launch/Haas.jpg",
+    color: "#B6BABD"
+  }
 };
 
+
 const driverPhotos = {
-  "max_verstappen": "https://media.formula1.com/digital-assets/drivers/2024/MAX.jpg",
-  "lewis_hamilton": "https://media.formula1.com/digital-assets/drivers/2024/LEW.jpg",
-  "charles_leclerc": "https://media.formula1.com/digital-assets/drivers/2024/LEC.jpg",
-  "lando_norris": "https://media.formula1.com/digital-assets/drivers/2024/NOR.jpg",
-  "fernando_alonso": "https://media.formula1.com/digital-assets/drivers/2024/ALO.jpg",
-  "george_russell": "https://media.formula1.com/digital-assets/drivers/2024/RUS.jpg"
+  "max_verstappen": "https://media.formula1.com/digital-assets/drivers/2025/MAX.jpg",
+  "sergio_perez": "https://media.formula1.com/digital-assets/drivers/2025/PER.jpg",
+  "lewis_hamilton": "https://media.formula1.com/digital-assets/drivers/2025/LEW.jpg",
+  "george_russell": "https://media.formula1.com/digital-assets/drivers/2025/RUS.jpg",
+  "charles_leclerc": "https://media.formula1.com/digital-assets/drivers/2025/LEC.jpg",
+  "carlos_sainz": "https://media.formula1.com/digital-assets/drivers/2025/SAI.jpg",
+  "lando_norris": "https://media.formula1.com/digital-assets/drivers/2025/NOR.jpg",
+  "oscar_piastri": "https://media.formula1.com/digital-assets/drivers/2025/PIA.jpg",
+  "fernando_alonso": "https://media.formula1.com/digital-assets/drivers/2025/ALO.jpg",
+  "lance_stroll": "https://media.formula1.com/digital-assets/drivers/2025/STR.jpg",
+  "esteban_ocon": "https://media.formula1.com/digital-assets/drivers/2025/OCO.jpg",
+  "pierre_gasly": "https://media.formula1.com/digital-assets/drivers/2025/GAS.jpg",
+  "yuki_tsunoda": "https://media.formula1.com/digital-assets/drivers/2025/TSU.jpg",
+  "daniel_ricciardo": "https://media.formula1.com/digital-assets/drivers/2025/RIC.jpg",
+  "alex_albon": "https://media.formula1.com/digital-assets/drivers/2025/ALB.jpg",
+  "logan_sargeant": "https://media.formula1.com/digital-assets/drivers/2025/SAR.jpg",
+  "zhou_guanyu": "https://media.formula1.com/digital-assets/drivers/2025/ZHO.jpg",
+  "valtteri_bottas": "https://media.formula1.com/digital-assets/drivers/2025/BOT.jpg",
+  "nico_hulkenberg": "https://media.formula1.com/digital-assets/drivers/2025/HUL.jpg",
+  "kevin_magnussen": "https://media.formula1.com/digital-assets/drivers/2025/MAG.jpg"
 };
+
 
 const fallbackData = {
   constructorStandings: [
